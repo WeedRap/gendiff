f9e1251fb9d1f9f91e86fba26461455a7795983a
WeedRap <1337rafik.weed@gmail.com>
2024-01-18 18:45:19 +0300

    Add cli.rs using clap

---
+++ src/cli.rs
@@ -0,0 +1,21 @@
+use clap::{command, Arg};
+
+pub fn parse_args() -> (String, String, String) {
+    let matches = command!()
+        .about("Compares two configuration files and shows a difference.")
+        .arg(Arg::new("first_file").required(true))
+        .arg(Arg::new("second_file").required(true))
+        .arg(
+            Arg::new("format")
+                .help("Set the format of output")
+                .short('f')
+                .long("format")
+                .default_value(""),
+        )
+        .get_matches();
+
+    let first_file = matches.get_one::<String>("first_file").unwrap();
+    let second_file = matches.get_one::<String>("second_file").unwrap();
+    let output_format = matches.get_one::<String>("format").unwrap();
+    (first_file.to_string(), second_file.to_string(), output_format.to_string())
+}
