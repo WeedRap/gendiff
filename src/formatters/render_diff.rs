419fa067481e4c526e573d2e4a965ee1b62144f8
WeedRap <1337rafik.weed@gmail.com>
2024-01-18 20:42:48 +0300

    Update render_diff

--- src/formatters/render_diff.rs
+++ src/formatters/render_diff.rs
@@ -1,10 +1,10 @@
-use crate::diff_tree::DiffNode;
-use crate::formatters::plain::render_plain;
-use crate::formatters::stylish::to_stylish;
+use crate::{
+    diff_tree::DiffNode, formatters::plain::render_plain, formatters::stylish::render_stylish,
+};
 
 pub fn render_diff(diff: &Vec<DiffNode>, format_name: String) -> String {
     match format_name.as_str() {
-        "stylish" => to_stylish(diff),
+        "stylish" => render_stylish(diff),
         "plain" => render_plain(diff),
         _ => panic!(""),
     }
